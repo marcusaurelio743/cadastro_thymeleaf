6918e8452e488c47abc35c98ab4d2b4f5de79651
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2026-06-13 09:50:18 -0300

    criando o projeto

---
+++ cadastro-thymeleaf/src/main/java/cadastro/CadastroApplication.java
@@ -0,0 +1,15 @@
+package cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(CadastroApplication.class, args);
+
+	}
+
+}
